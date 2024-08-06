7eaa600f238a7ab8eb9f0775b342a867ee37227e
IT Compliance <rhodaarthur48@gmail.com>
2024-08-06 16:37:19 -0700

    completes home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { ThemeService } from './services/theme.service';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, HomePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
