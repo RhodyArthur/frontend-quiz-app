ce82eee70df1ea6cd53a74592ce963b2a684ebfe
RhodyArthur <rhodaarthur48@gmail.com>
2024-08-09 00:22:28 +0100

    completes home page functionalities

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { ThemeService } from '../../services/theme.service';
 import { CategoryService } from '../../services/category.service';
+import { response } from '../../interfaces/response';
+import { QuizService } from '../../services/quiz.service';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -10,18 +14,44 @@ import { CategoryService } from '../../services/category.service';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  quizData: response={quizzes:[]};
 
   selectedCategory: string | null = null;
+  showCategoryContainer: boolean = false;
 
   constructor(public themeService: ThemeService,
-              private categoryService: CategoryService
+              private categoryService: CategoryService,
+              private router: Router,
+              private activatedRoute: ActivatedRoute,
+              private quizService: QuizService
   ) {}
 
   ngOnInit() {
     this.categoryService.selectedCategory$
     .subscribe(category => {
       this.selectedCategory = category;
+      this.checkRoute()
     })
+
+    this.quizService.fetchedData()
+    .subscribe(data => {
+      this.quizData = data;
+    })
+
+    this.router.events
+    .pipe(filter(event => event instanceof NavigationEnd))
+    .subscribe(() => {
+      this.checkRoute();
+    });
+  }
+
+  checkRoute() {
+    const currentRoute = this.router.url;
+    this.showCategoryContainer = currentRoute !== '/' && !!this.selectedCategory;
+  }
+
+  toggleCategory(category: string) {
+    this.categoryService.setSelectedCategory(category)
   }
 
   toggleTheme(){
--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -5,13 +5,20 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root'
 })
 export class CategoryService {
+  private readonly localStorageKey = 'selectedCategory'
+  private selectedCategorySubject = new BehaviorSubject<string|null>(localStorage.getItem(this.localStorageKey))
+  selectedCategory$ = this.selectedCategorySubject.asObservable();
 
   constructor() { }
 
-  private selectedCategorySubject = new BehaviorSubject<string|null>(null)
-  selectedCategory$ = this.selectedCategorySubject.asObservable();
   
   setSelectedCategory(category: string) {
     this.selectedCategorySubject.next(category)
+    if(category){
+      localStorage.setItem(this.localStorageKey, category)
+    }
+    else {
+      localStorage.removeItem(this.localStorageKey)
+    }
   }
 }
